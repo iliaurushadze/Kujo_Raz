e8025f4f81b974436d4324f7233f4e82e029f80b
iliaurushadze8 <iliaurushadze24@gmail.com>
2023-04-15 14:40:08 +0400

    Added Data Anottations.

--- Kujo_RazWeb/Model/Category.cs
+++ Kujo_RazWeb/Model/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace Kujo_RazWeb.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kujo_RazWeb.Model
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
