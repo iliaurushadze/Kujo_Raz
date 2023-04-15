282eaafb3929047fac3e7be06b6d40eae0ae9fa9
iliaurushadze8 <iliaurushadze24@gmail.com>
2023-04-15 14:38:39 +0400

    Added Category Model.

---
+++ Kujo_RazWeb/Model/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Kujo_RazWeb.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
