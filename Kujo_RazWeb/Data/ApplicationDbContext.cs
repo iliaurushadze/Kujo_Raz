15d4dad7a7225e193c141bec8a151ea689c76fb2
iliaurushadze8 <iliaurushadze24@gmail.com>
2023-04-16 14:13:41 +0400

    Added ApplicationDbContext.

---
+++ Kujo_RazWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Kujo_RazWeb.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kujo_RazWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Category { get; set; }
+    }
+}
