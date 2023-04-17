e19dfc50b4fb49b87d960b1ca0c3e51e88455991
iliaurushadze8 <iliaurushadze24@gmail.com>
2023-04-17 13:49:37 +0400

    Added Create Category UI.

---
+++ Kujo_RazWeb/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kujo_RazWeb.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
