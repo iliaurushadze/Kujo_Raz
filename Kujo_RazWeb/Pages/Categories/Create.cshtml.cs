c52cef03501ba22f002a608b3026c4e53df3378d
iliaurushadze8 <iliaurushadze24@gmail.com>
2023-04-17 13:59:18 +0400

    Added Create Property In Page Model.

--- Kujo_RazWeb/Pages/Categories/Create.cshtml.cs
+++ Kujo_RazWeb/Pages/Categories/Create.cshtml.cs
@@ -1,3 +1,4 @@
+using Kujo_RazWeb.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +6,7 @@ namespace Kujo_RazWeb.Pages.Categories
 {
     public class CreateModel : PageModel
     {
+        public Category Category { get; set; }
         public void OnGet()
         {
         }
