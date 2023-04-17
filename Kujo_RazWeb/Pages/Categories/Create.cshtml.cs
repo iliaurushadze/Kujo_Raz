b886e1f66a87150b43ee4bc6a90d0fdaa6a8970c
iliaurushadze8 <iliaurushadze24@gmail.com>
2023-04-17 15:38:16 +0400

    Added Custom Validation.

--- Kujo_RazWeb/Pages/Categories/Create.cshtml.cs
+++ Kujo_RazWeb/Pages/Categories/Create.cshtml.cs
@@ -20,6 +20,10 @@ namespace Kujo_RazWeb.Pages.Categories
 
         public async Task<IActionResult> OnPost()
         {
+            if(Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError(string.Empty, "The DisplayOrder cannot exactly match the Name.");
+            }
             if(ModelState.IsValid)
             { 
                 await _db.Category.AddAsync(Category);
