c2e8d3d08e34853c69150a936b92e7ca22794572
iliaurushadze8 <iliaurushadze24@gmail.com>
2023-04-18 12:49:00 +0400

    Added Edit Category.

---
+++ Kujo_RazWeb/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,39 @@
+using Kujo_RazWeb.Data;
+using Kujo_RazWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kujo_RazWeb.Pages.Categories
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty] // Instead of writing category variable, as OnPost method's parameter.
+        public Category Category { get; set; }
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int id)
+        {
+            Category = _db.Category.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if(Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Category.Name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if(ModelState.IsValid)
+            { 
+                _db.Category.Update(Category);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+
+    
+}
