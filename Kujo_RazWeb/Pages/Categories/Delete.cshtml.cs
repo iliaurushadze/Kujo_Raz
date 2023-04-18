7b6cb734172b305157d5c8a02bc1ef7c77337918
iliaurushadze8 <iliaurushadze24@gmail.com>
2023-04-18 13:57:04 +0400

    Added Delete Category.

---
+++ Kujo_RazWeb/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,37 @@
+using Kujo_RazWeb.Data;
+using Kujo_RazWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kujo_RazWeb.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty] // Instead of writing category variable, as OnPost method's parameter.
+        public Category Category { get; set; }
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int id)
+        {
+            Category = _db.Category.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+                var categoryFromDb = _db.Category.Find(Category.Id);
+                if(categoryFromDb != null)
+                {
+                    _db.Category.Remove(categoryFromDb);
+                    await _db.SaveChangesAsync();
+                    return RedirectToPage("Index");
+                }
+
+            return Page();
+        }
+    }
+
+    
+}
